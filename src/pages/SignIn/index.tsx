24d30e1cf6ec5cad5329616c1f36525884697c8c
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-04 09:44:42 -0300

    Densidade de pixel

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
+
+import logoimg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <>
-      <Container />
+      <Container>
+        <Image source={logoimg} />
+      </Container>
     </>
   );
 };
