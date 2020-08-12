6c61901239557243bdd57105b29bfc7796bf8dd6
Mateus <mateus.pegoraro@perceptvision.com.br>
2020-08-12 13:07:59 -0300

    Cadastro na aplicação

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -12,8 +12,9 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-
 import Icon from 'react-native-vector-icons/Feather';
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -51,9 +52,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.',
+      );
+
+      navigation.navigate('SignIn');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
